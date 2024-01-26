b631f21e06910ba0ff4b43b5f948cd378c5597fe
DDDyylan <jieqiliu020105@gmail.com>
2024-01-26 23:42:32 +0000

    add abstract

---
+++ models/abstract_models.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+import torch.nn as nn
+
+class VAE_Abs(ABC, nn.Module):
+
+    @abstractmethod
+    def __init__(self, encoder, decoder, tokenizer_encoder, tokenizer_decoder, latent_size, output_dir, device=None):
+        pass
+
+    @abstractmethod
+    def forward(self, inputs, labels):
+        pass
+
+class DDPM_Abs(ABC, nn.Module):
+
+    @abstractmethod
+    def __init__(self, eps_model, betas, n_T, criterion):
+        pass
+
+    @abstractmethod
+    def forward(self, x, mu):
+        pass
+
+    @abstractmethod
+    def sample(self, n_sample, size, device):
+        pass
+
+class VAE_DDPM_Abs(ABC, nn.Module):
+
+    @abstractmethod
+    def __init__(self, model_vae, ddpm, ddpm_weight):
+        pass
+    
+    @abstractmethod
+    def forward(self, inputs, labels):
+        pass
--- test_train.py
+++ test_train.py
@@ -75,7 +75,7 @@ def main():
           train_epoch = 5, gradient_accumulation_steps = 1, device = 'cuda',
           fp16=False, fp16_opt_level=None, learning_rate=9e-5, adam_epsilon=1e-5,
           lr_end_multiplier= 0.01, power=3.0, warmup_steps=0, 
-          disable_bar=True, model_ppl=None, tokenizer_ppl=None, max_grad_norm=1, evaluate_during_training=True,
+          disable_bar=True, model_ppl=None, tokenizer_ppl=None, max_grad_norm=1, evaluate_during_training=False,
           no_save=True)
     print("training_done")
 
