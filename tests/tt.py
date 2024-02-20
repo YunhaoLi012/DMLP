13c62d5207a64a774a9988cc73f4244ff46c66ba
Li <yul080@dhoni.ucsd.edu>
2024-02-20 03:45:54 +0000

    QA-data-preprocessing

--- tests/tt.py
+++ tests/tt.py
@@ -1,3 +1,5 @@
-from DMLP.train.train_function import check
+from datasets import load_dataset,concatenate_datasets, Dataset,DatasetDict
 
-check()
+
+train_eval_dataset =load_dataset("guangyil/yelp_short_v2")
+print("done")
